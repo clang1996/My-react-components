766d383cd0aad39c69d18cfe0fb0d3b14e298257
你çclang1996 <>
2020-06-15 23:45:25 +0800

    Modal样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {RipleButton} from './components/RipleButton';
+import {Modal} from './components/Modal';
 
 function App() {
 		return (
 			<div>
 					<RipleButton/>
+					<Modal/>
 			</div>
 		);
 }
